89851f95e2523caf0483c6bf11615074073e3fe1
Travis Petersen <travis.e.petersen@gmail.com>
2017-11-08 01:21:17 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -5,4 +5,23 @@ class Transfer
     @sender, @receiver, @amount, @status = sender, receiver, amount, "pending"
   end
 
+  def valid?
+    self.sender.valid? && self.receiver.valid? && self.status == "pending" ? true : false
+  end
+
+  def execute_transaction
+    if self.valid?
+      self.sender.balance -= amount
+      self.receiver.deposit(amount)
+      self.status = "executed"
+    else
+      puts "The sender's account is invalid."
+    end
+  end
+
+  def reverse_transfer
+    self.new(self.receiver, self.sender, self.amount)
+    self.execute_transcation
+  end
+
 end
